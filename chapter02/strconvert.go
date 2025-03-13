455c47cf65fabb7cf03f7d82082e85be37d1d105
gmq1990 <gmqlive@gmail.com>
2025-03-13 21:33:20 +0800

    modified:   chapter02/strconvert.go

--- chapter02/strconvert.go
+++ chapter02/strconvert.go
@@ -27,8 +27,22 @@ func strConvert() {
 		fmt.Printf("%T %#v\n", v, v) // int64 100
 	}
 
-	// => float
+	// => float (parsefloat,指定目标int位数)
 	if v, err := strconv.ParseFloat("1.1", 64); err == nil {
 		fmt.Printf("%T %#v\n", v, v) // float64 1.1
 	}
+
+	//  其他类型 => string
+	sf := fmt.Sprintf("%d", 12)
+	fmt.Println(sf)
+
+	sf = fmt.Sprintf("%f", 12.01)
+	fmt.Printf("%q\n", sf)
+
+	fmt.Printf("%q\n", strconv.FormatBool(false))
+	fmt.Printf("%q\n", strconv.Itoa(12))
+	// 指定进制数
+	fmt.Printf("%q\n", strconv.FormatInt(12, 16))
+	// 指定科学计数、原有精度、float64位
+	fmt.Printf("%q\n", strconv.FormatFloat(10.1, 'E', -1, 64))
 }
